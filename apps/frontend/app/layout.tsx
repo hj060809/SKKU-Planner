630778a5d4ebacc1ed749861387b8adccc45b73a
hj060809 <hjbanf89@gmail.com>
2026-06-17 16:32:54 +0000

    fix(fe): ci error

--- apps/frontend/app/layout.tsx
+++ apps/frontend/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-// @ts-ignore
+// @ts-expect-error
 import "./globals.css";
 import { ClientApolloProvider } from "@/components/ApolloProvider";
 
